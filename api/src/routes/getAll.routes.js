c419a8c771f6a9d836196ae477bd8e9d0e3c1ba2
Rafael Fuentes <rafaeltiwifuentes@gmail.com>
2024-02-16 09:39:58 -0700

    getting a the sched for a single day

--- api/src/routes/getAll.routes.js
+++ api/src/routes/getAll.routes.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const Appointment = require('../models/appointment.model');
+const moment = require('moment')
 
 router.get('/', async (req, res) => {
+
+    const startOfDay = moment(req.body.date, "DD/MM/YYYY").startOf('day');
+    const endOfDay = moment(req.body.date, "DD/MM/YYYY").endOf('day');
+
+
+
+
+
     try {
-        const appointments = await Appointment.find({});
-        res.status(200).json(appointments);
+        const appointments = await Appointment.find({
+            date:  { $gte: startOfDay.toDate(), $lte: endOfDay.toDate() }
+        });
+res.status(200).json(appointments);
     } catch (error) {
-        res.status(500).send("Error al obtener las citas: " + error.message);
-    }
+    res.status(500).send("Error al obtener las citas: " + error.message);
+}
 });
 
 module.exports = router;
